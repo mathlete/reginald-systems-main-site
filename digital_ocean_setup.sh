368b9435a2630be6d0695c93c44475107a4f7c4e
mathlete <jr.mathlete@gmail.com>
2021-12-04 04:06:20 +0000

    fixed typo in deployment file

--- digital_ocean_setup.sh
+++ digital_ocean_setup.sh
@@ -1,5 +1,5 @@
 # setup node and compile
-npm install --prefix ./main.vue > /dev/null 2>&1 && npm run build --prefix ./main-vue > /dev/null 2>&1
+npm install --prefix ./main-vue > /dev/null 2>&1 && npm run build --prefix ./main-vue > /dev/null 2>&1
 # copy build to nginx folder
 cp -r main-vue/dist/* /var/www/html/
 # clean the current working directory of build artifacts
